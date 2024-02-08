b4895a43404a6b9bd0979d5173adcf085020fd84
Camila Correa <camilacorreasouza@gmail.com>
2024-02-08 12:37:49 -0300

    config: Adding pre-commit to Project

---
+++ example.py
@@ -0,0 +1,12 @@
+def my_func():
+    print('Ola')
+
+#Bibliotecas utilizadas:
+# Virtualenv -> Criar um ambiente virtual
+    # Habilitar o ambiente virtual: digitar no terminal -> .venv\Scripts\activate
+# Pylint -> Revisão do código
+# pre-commit -> Atrelar ao github e executar ações (hooks)
+
+# Pylint
+# snake_case -> Funções, Variáveis, Métodos
+# PascalCase -> Classes
